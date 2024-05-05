58b32b48d96327f51f2b6b2efc3e9f78742bb75e
kloutkake <112651256+kloutkake@users.noreply.github.com>
2024-05-05 15:13:32 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { Navbar } from "./components/Navbar";
 export default function Home() {
   return (
     <body>
-      <div>
-        <main className="text-white sm:p-10">
-          <h1>Welcome to my website!</h1> 
+      <div className="sm:p-10">
+        <main  className="sm:p-10">
+          <h1>Welcome to my website!</h1>
         </main>
         <p>The main purpose of this website is to be a blog for myself personally, CTF write-ups, to showcase my partners art, and a gallery for various interest I have (mainly my motorcycle).</p>
         <p>Of course my website is not looking great right now, but it is a work in progress! It will be a side project to work, CTFs, certification study, and my personal life.</p>
